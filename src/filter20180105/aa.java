7cc7516521c6988508c19892d13bc94f1091262f
DXX <1084934818@qq.com>
2018-01-09 10:59:21 +0800

    修改

--- src/filter20180105/aa.java
+++ src/filter20180105/aa.java
@@ -1,7 +1,20 @@
 package filter20180105;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class aa extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
 
 }
